b4eededa5ebd03003c8d05db410866c4ea533461
ToniZamour <92550909+ToniZamour@users.noreply.github.com>
2021-12-06 08:44:21 -0800

    Add files via upload

---
+++ Changes.lua
@@ -0,0 +1,298 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("KinderC", "Sentinel")
+local Main = Window:NewTab("Teleports")
+local MainSection = Main:NewSection("Ember")
+MainSection:NewButton("Raion Akuma", "", function()
+    teleportWorld('Raion Akuma')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Raion Akuma").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Dio Senko", "", function()
+    teleportWorld('Dio Senko')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Dio Senko").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Pts Raion", "", function()
+    teleportWorld('Pts Raion')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Pts Raion").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Bankai Akuma", "", function()
+    teleportWorld('Bankai Akuma')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Bankai Akuma").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Satori Akuma", "", function()
+    teleportWorld('Satori Akuma')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Satori Akuma").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Forged Akuma", "", function()
+    teleportWorld('Forged Akuma')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Forged Akuma").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+
+local MainSection = Main:NewSection("Nimbus")
+MainSection:NewButton("Ashen Storm", "", function()
+    teleportWorld('Ashen Storm')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Ashen Storm").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Xeno Dokei", "", function()
+    teleportWorld('Xeno Dokei')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Xeno Dokei").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Giovanni Shizen", "", function()
+    teleportWorld('Giovanni Shizen')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Giovanni Shizen").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Odin Saberu", "", function()
+    teleportWorld('Odin Saberu')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Odin Saberu").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+
+local MainSection = Main:NewSection("Obelisk")
+MainSection:NewButton("Jizo Seishin", "", function()
+    teleportWorld('Jizo Seishin')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Jizo Seishin").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Kabu Kobra", "", function()
+    teleportWorld('Kabu Kobra')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Kabu Cobra").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Satoru Okami", "", function()
+    teleportWorld('Satoru Okami')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Satoru Okami").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Arahaki Jokei", "", function()
+    teleportWorld('Arahaki Jokei')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Arahaki Jokei").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Light Jokei", "", function()
+    teleportWorld('Light Jokei')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Light Jokei").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+
+local MainSection = Main:NewSection("Training")
+MainSection:NewButton("Ryuji Kenichi", "", function()
+    teleportWorld('Ryuji Kenichi')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Ryuji Kenichi").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Bruce Kenichi", "", function()
+    teleportWorld('Bruce Kenichi')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Bruce Kenichi").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+
+local MainSection = Main:NewSection("Bridge")
+MainSection:NewButton("Jayramaki", "", function()
+    teleportWorld('Jayramaki')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Jayramaki").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Borumaki", "", function()
+    teleportWorld('Borumaki')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Borumaki").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Narumaki", "", function()
+    teleportWorld('Narumaki')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Narumaki").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+MainSection:NewButton("Minakaze", "", function()
+    teleportWorld('Minakaze')
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer
+    }
+    
+    workspace.bossdropmission.missions:FindFirstChild("Minakaze").missiongiver.CLIENTTALK:FireServer(unpack(args))
+end)
+local RankUP = Window:NewTab("Rank UP")
+local RankUPSection = RankUP:NewSection("Levels")
+RankUPSection:NewButton("RankUP", "Teleport TO Rankup", function()
+    teleportRank('Part')
+end)
+RankUPSection:NewToggle("KillAuro", "", function()
+    local args = {
+        [1] = "target",
+        [2] = getNil("npc4", "Model").HumanoidRootPart
+    }
+    
+    game:GetService("Players").LocalPlayer.startevent:FireServer(unpack(args))
+end)
+
+function teleportTO(placeCFrame)
+    local plyr = game.Players.LocalPlayer;
+    if plyr.Character then
+        plyr.Character.HumanoidRootPart.CFrame = placeCFrame;
+    end
+end
+
+function teleportWorld(world)
+    if  game:GetService("Workspace").bossdropmission.missions:FindFirstChild(world) then
+        teleportTO(game:GetService("Workspace").bossdropmission.missions[world].missiongiver.Part.CFrame)
+    end
+end
+RankUPSection:NewToggle("NPC Mission", "", function()
+    function teleportBODY(npc)
+        if game:GetService("Workspace").missiongivers:FindFirstChild(npc) then
+            teleportTO(game:GetService("Workspace").missiongivers[npc].Part.CFrame)
+        end
+    end
+    teleportBODY('')
+    wait(1)
+    teleportBODY('')
+    wait(1)
+    teleportBODY('')
+end)
+local Equip = Window:NewTab("Equip")
+local EquipSection = Equip:NewSection("Modes")
+EquipSection:NewDropdown("Equip tailed", "", {"gates", "sevenheaven", "cursemark", "lightningmode","kaguyamode","sandmode","uzumakimode","namikazemode","karmanimbus","weaponart","jojo","tentail","papermode","snakesage","slugsage","blacklightningmode","claymode","toadsage","hoshikagimode","kamizurumode","sarutobimode","arenapowerup","demonshroud","tail1","tail2","tail3","tail4","tail5","tail6","tail7","tail8","tail9","tentail","tail1x","tail2x","tail3x","tail4x","tail5x","tail6x","tail7x","tail8x","tail9x","tentailx","karmaember","karmahaze","karmaobelisk","bankaiinfernomode","karmadunes","santastand","toadsageV2","cursemark2","captainjokei","cursemark3","akumahand","rabbit","bunnysage","raionrengokumode","mechaspiritmode","gammacurse","gomugomuno","UltraInstinct","theone","forgerengokumode","forgesengokumode","gunderadseals","ryujikenichimode","ryujikenichiwhitemode","raionazuremode","raionsengokumode","satorigoldmode","riserinfernomode"}, function(v)
+    local args = {
+        [1] = "equipmode",
+        [2] = game:GetService("ReplicatedStorage").alljutsu.modes[v],
+        [3] = ""
+    }
+    
+    game:GetService("Players").LocalPlayer.startevent:FireServer(unpack(args))
+end)
+local Server = Window:NewTab("Server")
+local ServerSection = Server:Neection("Servers")
+ServerSection:NewButton("Join", "Join a private server", {"_30BAS","cosaBF","1e5hNE","JbZWa6","VQzbLi","NQK-WF","3ATcsf"}, function(v)
+    local args = {
+        [1] = "teleporttoprivate",
+        [2] = v,
+        [3] = game:GetService("Players").LocalPlayer.PlayerGui.Main.ingame.Menu.ArenaTab.teleport.teleportprivate.mapselected
+    }
+    
+    game:GetService("Players").LocalPlayer.startevent:FireServer(unpack(args))
+end)
+ServerSection:NewButton("Create", "Create a private server", function()
+    local args = {
+        [1] = "createprivateserver",
+        [2] = 4601350214
+    }
+    
+    game:GetService("Players").LocalPlayer.startevent:FireServer(unpack(args))
+end)
+
+
+--------------------Rank UP
+
+
+function teleportRank(rank)
+    if game:GetService("Workspace").rank:FindFirstChild(rank) then
+        teleportTO(game:GetService("Workspace").rank.Part.CFrame)
+    end
+end
+teleportTO('Part')
+
+
+
+------NPC MIssions
+
+function teleportBODY(npc)
+        if game:GetService("Workspace").missiongivers:FindFirstChild(npc) then
+            teleportTO(game:GetService("Workspace").missiongivers[npc].Part.CFrame)
+        end
+    end
+    
+teleportBODY('')
+
+
+-- Script generated by SimpleSpy - credits to exx#9394
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+workspace.missiongivers:FindFirstChild("")..CLIENTTALK:FireServer(unpack(args))
+
+-- Script generated by SimpleSpy - credits to exx#9394
+
+local args = {
+    [1] = "key",
+    [2] = "e"
+}
+
+game:GetService("Players").LocalPlayer.Character.combat.update:FireServer(unpack(args))
+
+-- Script generated by SimpleSpy - credits to exx#9394
+
+local args = {
+    [1] = "createprivateserver",
+    [2] = 4601350214
+}
+
+game:GetService("Players").LocalPlayer.startevent:FireServer(unpack(args))
